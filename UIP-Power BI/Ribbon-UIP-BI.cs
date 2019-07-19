9fa2a32eb3292bb52f7d2ba4d5f31fd948ad612e
Camron Walker <camronwalker@gmail.com>
2019-07-19 17:03:07 -0600

    Added a few buttons / Formatted Excel file.

---
+++ UIP-Power BI/Ribbon-UIP-BI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace UIP_Power_BI
+{
+    public partial class Ribbon_UIP_BI
+    {
+        private void Ribbon_UIP_BI_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void Add_New_Trade_Button_Click(object sender, RibbonControlEventArgs e)
+        {
+            Globals.ThisWorkbook.Application.Run("Test");
+        }
+    }
+}
